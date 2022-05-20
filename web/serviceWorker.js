2c03725787918a450c25ab861318c19b036d8b62
jshwang-uniark <sy_kim@uniark.co.jp>
2022-05-20 14:12:30 +0900

    Update serviceWorker.js

--- web/serviceWorker.js
+++ web/serviceWorker.js
@@ -46,22 +46,6 @@ self.addEventListener('activate', function (e) {
 	);
 });
 
-self.addEventListener('fetch', function (e) {
-	e.respondWith(
-		caches.match(e.request).then(function (r) {
-			console.log('[Service Worker] Fetching resource: ', e.request.url);
-			if (r) return r;
-
-			return fetch(e.request).then(function (response) {
-				return caches.open(cacheName).then(function (cache) {
-					console.log('[Service Worker] Caching new resource: ' + e.request.url);
-					cache.put(e.request, response.clone());
-					return response;
-				});
-			});
-		})
-	);
-});
 
 
 
